a44bc668d6d6b897b32f4953574ab5c44f041bc5
mrdacuy <125877019+mrdacuy@users.noreply.github.com>
2023-06-19 18:30:39 +0700

    THAY DOI CONG TY

--- KskSoft/FrmCongTy.Designer.vb
+++ KskSoft/FrmCongTy.Designer.vb
@@ -19,74 +19,32 @@ Partial Class FrmCongTy
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim EditorButtonImageOptions1 As DevExpress.XtraEditors.Controls.EditorButtonImageOptions = New DevExpress.XtraEditors.Controls.EditorButtonImageOptions()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmCongTy))
+        Dim SerializableAppearanceObject1 As DevExpress.Utils.SerializableAppearanceObject = New DevExpress.Utils.SerializableAppearanceObject()
+        Dim SerializableAppearanceObject2 As DevExpress.Utils.SerializableAppearanceObject = New DevExpress.Utils.SerializableAppearanceObject()
+        Dim SerializableAppearanceObject3 As DevExpress.Utils.SerializableAppearanceObject = New DevExpress.Utils.SerializableAppearanceObject()
+        Dim SerializableAppearanceObject4 As DevExpress.Utils.SerializableAppearanceObject = New DevExpress.Utils.SerializableAppearanceObject()
+        Me.GridControl1 = New DevExpress.XtraGrid.GridControl()
+        Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.BtnSua = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.RepositoryItemButtonEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemButtonEdit()
+        Me.NFrmCongTy = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.NfrmCongTyId = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.SimpleButton2 = New DevExpress.XtraEditors.SimpleButton()
         Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
         Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
         Me.TextEdit1 = New DevExpress.XtraEditors.TextEdit()
-        Me.GridControl1 = New DevExpress.XtraGrid.GridControl()
-        Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-        Me.NFrmCongTy = New DevExpress.XtraGrid.Columns.GridColumn()
-        Me.NfrmCongTyId = New DevExpress.XtraGrid.Columns.GridColumn()
+        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RepositoryItemButtonEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SplitContainer1.Panel1.SuspendLayout()
         Me.SplitContainer1.Panel2.SuspendLayout()
         Me.SplitContainer1.SuspendLayout()
         CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'SplitContainer1
-        '
-        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
-        Me.SplitContainer1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
-        Me.SplitContainer1.Name = "SplitContainer1"
-        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
-        '
-        'SplitContainer1.Panel1
-        '
-        Me.SplitContainer1.Panel1.Controls.Add(Me.SimpleButton1)
-        Me.SplitContainer1.Panel1.Controls.Add(Me.LabelControl1)
-        Me.SplitContainer1.Panel1.Controls.Add(Me.TextEdit1)
-        '
-        'SplitContainer1.Panel2
-        '
-        Me.SplitContainer1.Panel2.Controls.Add(Me.GridControl1)
-        Me.SplitContainer1.Size = New System.Drawing.Size(699, 454)
-        Me.SplitContainer1.SplitterDistance = 45
-        Me.SplitContainer1.SplitterWidth = 5
-        Me.SplitContainer1.TabIndex = 0
-        '
-        'SimpleButton1
-        '
-        Me.SimpleButton1.Location = New System.Drawing.Point(373, 9)
-        Me.SimpleButton1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
-        Me.SimpleButton1.Name = "SimpleButton1"
-        Me.SimpleButton1.Size = New System.Drawing.Size(93, 22)
-        Me.SimpleButton1.TabIndex = 2
-        Me.SimpleButton1.Text = "Lưu"
-        '
-        'LabelControl1
-        '
-        Me.LabelControl1.Appearance.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LabelControl1.Appearance.Options.UseFont = True
-        Me.LabelControl1.Location = New System.Drawing.Point(12, 11)
-        Me.LabelControl1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
-        Me.LabelControl1.Name = "LabelControl1"
-        Me.LabelControl1.Size = New System.Drawing.Size(78, 16)
-        Me.LabelControl1.TabIndex = 1
-        Me.LabelControl1.Text = "Tên Công Ty:"
-        '
-        'TextEdit1
-        '
-        Me.TextEdit1.Location = New System.Drawing.Point(108, 8)
-        Me.TextEdit1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
-        Me.TextEdit1.Name = "TextEdit1"
-        Me.TextEdit1.Properties.Appearance.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextEdit1.Properties.Appearance.Options.UseFont = True
-        Me.TextEdit1.Size = New System.Drawing.Size(260, 22)
-        Me.TextEdit1.TabIndex = 0
         '
         'GridControl1
         '
@@ -96,66 +54,163 @@ Partial Class FrmCongTy
         Me.GridControl1.MainView = Me.GridView1
         Me.GridControl1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.GridControl1.Name = "GridControl1"
-        Me.GridControl1.Size = New System.Drawing.Size(699, 404)
+        Me.GridControl1.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.RepositoryItemButtonEdit1})
+        Me.GridControl1.Size = New System.Drawing.Size(697, 356)
         Me.GridControl1.TabIndex = 0
         Me.GridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView1})
         '
         'GridView1
         '
-        Me.GridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.NFrmCongTy, Me.NfrmCongTyId})
-        Me.GridView1.DetailHeight = 101
+        Me.GridView1.Appearance.HeaderPanel.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GridView1.Appearance.HeaderPanel.Options.UseFont = True
+        Me.GridView1.Appearance.Row.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GridView1.Appearance.Row.Options.UseFont = True
+        Me.GridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.BtnSua, Me.NFrmCongTy, Me.NfrmCongTyId})
+        Me.GridView1.DetailHeight = 54
         Me.GridView1.GridControl = Me.GridControl1
         Me.GridView1.Name = "GridView1"
+        Me.GridView1.OptionsBehavior.Editable = False
+        Me.GridView1.OptionsView.ColumnAutoWidth = False
+        Me.GridView1.OptionsView.ShowGroupPanel = False
+        '
+        'BtnSua
+        '
+        Me.BtnSua.Caption = "Sửa"
+        Me.BtnSua.ColumnEdit = Me.RepositoryItemButtonEdit1
+        Me.BtnSua.MinWidth = 13
+        Me.BtnSua.Name = "BtnSua"
+        Me.BtnSua.Visible = True
+        Me.BtnSua.VisibleIndex = 0
+        Me.BtnSua.Width = 96
+        '
+        'RepositoryItemButtonEdit1
+        '
+        Me.RepositoryItemButtonEdit1.AutoHeight = False
+        EditorButtonImageOptions1.Image = CType(resources.GetObject("EditorButtonImageOptions1.Image"), System.Drawing.Image)
+        Me.RepositoryItemButtonEdit1.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Glyph, "", -1, True, True, False, EditorButtonImageOptions1, New DevExpress.Utils.KeyShortcut(System.Windows.Forms.Keys.None), SerializableAppearanceObject1, SerializableAppearanceObject2, SerializableAppearanceObject3, SerializableAppearanceObject4, "", Nothing, Nothing, DevExpress.Utils.ToolTipAnchor.[Default])})
+        Me.RepositoryItemButtonEdit1.Name = "RepositoryItemButtonEdit1"
+        Me.RepositoryItemButtonEdit1.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.HideTextEditor
         '
         'NFrmCongTy
         '
         Me.NFrmCongTy.Caption = "CÔNG TY"
         Me.NFrmCongTy.FieldName = "Congty"
-        Me.NFrmCongTy.MinWidth = 17
+        Me.NFrmCongTy.MinWidth = 11
         Me.NFrmCongTy.Name = "NFrmCongTy"
         Me.NFrmCongTy.Visible = True
-        Me.NFrmCongTy.VisibleIndex = 1
-        Me.NFrmCongTy.Width = 574
+        Me.NFrmCongTy.VisibleIndex = 2
+        Me.NFrmCongTy.Width = 469
         '
         'NfrmCongTyId
         '
         Me.NfrmCongTyId.Caption = "ID"
         Me.NfrmCongTyId.FieldName = "Id"
-        Me.NfrmCongTyId.MinWidth = 17
+        Me.NfrmCongTyId.MinWidth = 11
         Me.NfrmCongTyId.Name = "NfrmCongTyId"
         Me.NfrmCongTyId.Visible = True
-        Me.NfrmCongTyId.VisibleIndex = 0
-        Me.NfrmCongTyId.Width = 107
+        Me.NfrmCongTyId.VisibleIndex = 1
+        Me.NfrmCongTyId.Width = 126
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
+        Me.SplitContainer1.IsSplitterFixed = True
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.SimpleButton2)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.SimpleButton1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.LabelControl1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.TextEdit1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.GridControl1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(697, 406)
+        Me.SplitContainer1.SplitterDistance = 45
+        Me.SplitContainer1.SplitterWidth = 5
+        Me.SplitContainer1.TabIndex = 1
+        '
+        'SimpleButton2
+        '
+        Me.SimpleButton2.Appearance.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SimpleButton2.Appearance.Options.UseFont = True
+        Me.SimpleButton2.ImageOptions.SvgImage = CType(resources.GetObject("SimpleButton2.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
+        Me.SimpleButton2.Location = New System.Drawing.Point(583, 5)
+        Me.SimpleButton2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.SimpleButton2.Name = "SimpleButton2"
+        Me.SimpleButton2.Size = New System.Drawing.Size(92, 32)
+        Me.SimpleButton2.TabIndex = 3
+        Me.SimpleButton2.Text = "Hủy"
+        '
+        'SimpleButton1
+        '
+        Me.SimpleButton1.Appearance.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SimpleButton1.Appearance.Options.UseFont = True
+        Me.SimpleButton1.ImageOptions.SvgImage = CType(resources.GetObject("SimpleButton1.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
+        Me.SimpleButton1.Location = New System.Drawing.Point(485, 5)
+        Me.SimpleButton1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.SimpleButton1.Name = "SimpleButton1"
+        Me.SimpleButton1.Size = New System.Drawing.Size(92, 32)
+        Me.SimpleButton1.TabIndex = 2
+        Me.SimpleButton1.Text = "Lưu"
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Appearance.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelControl1.Appearance.Options.UseFont = True
+        Me.LabelControl1.Location = New System.Drawing.Point(8, 11)
+        Me.LabelControl1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(78, 16)
+        Me.LabelControl1.TabIndex = 1
+        Me.LabelControl1.Text = "Tên Công Ty:"
+        '
+        'TextEdit1
+        '
+        Me.TextEdit1.Location = New System.Drawing.Point(92, 9)
+        Me.TextEdit1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TextEdit1.Name = "TextEdit1"
+        Me.TextEdit1.Properties.Appearance.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextEdit1.Properties.Appearance.Options.UseFont = True
+        Me.TextEdit1.Size = New System.Drawing.Size(387, 22)
+        Me.TextEdit1.TabIndex = 0
         '
         'FrmCongTy
         '
-        Me.Appearance.Options.UseFont = True
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(699, 454)
+        Me.ClientSize = New System.Drawing.Size(697, 406)
         Me.Controls.Add(Me.SplitContainer1)
-        Me.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "FrmCongTy"
         Me.Text = "FrmCongTy"
+        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RepositoryItemButtonEdit1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.SplitContainer1.Panel1.ResumeLayout(False)
         Me.SplitContainer1.Panel1.PerformLayout()
         Me.SplitContainer1.Panel2.ResumeLayout(False)
         CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.SplitContainer1.ResumeLayout(False)
         CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents GridControl1 As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents BtnSua As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents RepositoryItemButtonEdit1 As DevExpress.XtraEditors.Repository.RepositoryItemButtonEdit
+    Friend WithEvents NFrmCongTy As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents NfrmCongTyId As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents SplitContainer1 As SplitContainer
     Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
     Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
     Friend WithEvents TextEdit1 As DevExpress.XtraEditors.TextEdit
-    Friend WithEvents GridControl1 As DevExpress.XtraGrid.GridControl
-    Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-    Friend WithEvents NFrmCongTy As DevExpress.XtraGrid.Columns.GridColumn
-    Friend WithEvents NfrmCongTyId As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents SimpleButton2 As DevExpress.XtraEditors.SimpleButton
 End Class
--- KskSoft/FrmCongTy.vb
+++ KskSoft/FrmCongTy.vb
@@ -1,22 +1,29 @@
-﻿
-Imports System.Data.SqlClient
+﻿Imports System.Data.SqlClient
 Imports System.IO
-
+Imports DevExpress.XtraGrid.Views.Grid
 
 Public Class FrmCongTy
 
     Dim Dt As New DataSet
+    Dim IdSo As String
     Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles SimpleButton1.Click
-        Ket_noi()
-        Dim sql As String = "INSERT INTO Solieucongty(Congty) VALUES (@val1)"
-        Dim Cmd As New SqlCommand(sql, cnn)
-        Cmd.Parameters.AddWithValue("@val1", TextEdit1.EditValue)
-        Cmd.ExecuteNonQuery()
-        Dong_Ket_noi()
+        If IdSo = "" Then
+            Ket_noi()
+            Dim sql As String = "INSERT INTO Solieucongty(Congty) VALUES (@val1)"
+            Dim Cmd As New SqlCommand(sql, cnn)
+            Cmd.Parameters.AddWithValue("@val1", TextEdit1.EditValue)
+            Cmd.ExecuteNonQuery()
+            Dong_Ket_noi()
+            Load_CongTy()
+            TextEdit1.EditValue = ""
+            IdSo = ""
+        Else
+            Edit_DaTa()
+        End If
     End Sub
     Sub Load_CongTy()
-
         Ket_noi()
+        Dt.Clear()
         Dim strg As String = "SELECT * FROM Solieucongty"
         Dim Cmd As New SqlDataAdapter(strg, cnn)
 
@@ -27,5 +34,27 @@ Public Class FrmCongTy
 
     Private Sub FrmCongTy_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Load_CongTy()
+    End Sub
+
+    Private Sub GridView1_RowCellClick(sender As Object, e As RowCellClickEventArgs) Handles GridView1.RowCellClick
+        If e.Column.Name = "BtnSua" Then
+            TextEdit1.EditValue = GridView1.GetFocusedRowCellValue("Congty")
+            IdSo = GridView1.GetFocusedRowCellValue("Id")
+        End If
+    End Sub
+    Private Sub Edit_DaTa()
+        Ket_noi()
+        Dim Cmd As New SqlCommand("Update Solieucongty set Congty = @Congty where Id =" & IdSo & "", cnn)
+        Cmd.Parameters.AddWithValue("@Congty", TextEdit1.EditValue)
+        Cmd.ExecuteNonQuery()
+        Dong_Ket_noi()
+        TextEdit1.EditValue = ""
+        IdSo = ""
+        Load_CongTy()
+    End Sub
+
+    Private Sub SimpleButton2_Click(sender As Object, e As EventArgs) Handles SimpleButton2.Click
+        TextEdit1.EditValue = ""
+        IdSo = ""
     End Sub
 End Class
